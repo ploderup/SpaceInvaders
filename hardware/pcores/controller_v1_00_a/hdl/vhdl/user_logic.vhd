fe8d9368314f885516031a7ca52b5b7fb13734f1
asdfa <asdfakf>
2017-11-17 13:09:15 -0700

    Added IP for controller. Also edited VHDL for controller so as to generate interrupts and allow for acknowledgment from the INTC.

---
+++ hardware/pcores/controller_v1_00_a/hdl/vhdl/user_logic.vhd
@@ -0,0 +1,253 @@
+------------------------------------------------------------------------------
+-- user_logic.vhd - entity/architecture pair
+------------------------------------------------------------------------------
+-- Filename:          user_logic.vhd
+-- Version:           1.00.a
+-- Description:       User logic.
+-- Date:              Fri Nov 17 12:06:08 2017 (by Create and Import Peripheral Wizard)
+-- VHDL Standard:     VHDL'93
+------------------------------------------------------------------------------
+-- Naming Conventions:
+--   active low signals:                    "*_n"
+--   clock signals:                         "clk", "clk_div#", "clk_#x"
+--   reset signals:                         "rst", "rst_n"
+--   generics:                              "C_*"
+--   user defined types:                    "*_TYPE"
+--   state machine next state:              "*_ns"
+--   state machine current state:           "*_cs"
+--   combinatorial signals:                 "*_com"
+--   pipelined or register delay signals:   "*_d#"
+--   counter signals:                       "*cnt*"
+--   clock enable signals:                  "*_ce"
+--   internal version of output port:       "*_i"
+--   device pins:                           "*_pin"
+--   ports:                                 "- Names begin with Uppercase"
+--   processes:                             "*_PROCESS"
+--   component instantiations:              "<ENTITY_>I_<#|FUNC>"
+------------------------------------------------------------------------------
+
+-- DO NOT EDIT BELOW THIS LINE --------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+library proc_common_v3_00_a;
+use proc_common_v3_00_a.proc_common_pkg.all;
+
+-- DO NOT EDIT ABOVE THIS LINE --------------------
+
+--USER libraries added here
+
+------------------------------------------------------------------------------
+-- Entity section
+------------------------------------------------------------------------------
+-- Definition of Generics:
+--   C_NUM_REG                    -- Number of software accessible registers
+--   C_SLV_DWIDTH                 -- Slave interface data bus width
+--
+-- Definition of Ports:
+--   Bus2IP_Clk                   -- Bus to IP clock
+--   Bus2IP_Resetn                -- Bus to IP reset
+--   Bus2IP_Data                  -- Bus to IP data bus
+--   Bus2IP_BE                    -- Bus to IP byte enables
+--   Bus2IP_RdCE                  -- Bus to IP read chip enable
+--   Bus2IP_WrCE                  -- Bus to IP write chip enable
+--   IP2Bus_Data                  -- IP to Bus data bus
+--   IP2Bus_RdAck                 -- IP to Bus read transfer acknowledgement
+--   IP2Bus_WrAck                 -- IP to Bus write transfer acknowledgement
+--   IP2Bus_Error                 -- IP to Bus error response
+------------------------------------------------------------------------------
+
+entity user_logic is
+  generic
+  (
+    -- ADD USER GENERICS BELOW THIS LINE ---------------
+    UP_BIT		: integer := 0;
+	DOWN_BIT	: integer := 1;
+	LEFT_BIT	: integer := 2;
+	RIGHT_BIT	: integer := 3;
+	BUTTON_BIT 	: integer := 4;
+	ACK_BIT		: integer := 31;
+    -- ADD USER GENERICS ABOVE THIS LINE ---------------
+
+    -- DO NOT EDIT BELOW THIS LINE ---------------------
+    -- Bus protocol parameters, do not add to or delete
+    C_NUM_REG                      : integer              := 1;
+    C_SLV_DWIDTH                   : integer              := 32
+    -- DO NOT EDIT ABOVE THIS LINE ---------------------
+  );
+  port
+  (
+    -- ADD USER PORTS BELOW THIS LINE ------------------
+    PMOD_up			: in std_logic;
+	PMOD_down		: in std_logic;
+	PMOD_left		: in std_logic;
+	PMOD_right		: in std_logic;
+	PMOD_button		: in std_logic;
+	CTRL_int		: out std_logic;
+    -- ADD USER PORTS ABOVE THIS LINE ------------------
+
+    -- DO NOT EDIT BELOW THIS LINE ---------------------
+    -- Bus protocol ports, do not add to or delete
+    Bus2IP_Clk                     : in  std_logic;
+    Bus2IP_Resetn                  : in  std_logic;
+    Bus2IP_Data                    : in  std_logic_vector(C_SLV_DWIDTH-1 downto 0);
+    Bus2IP_BE                      : in  std_logic_vector(C_SLV_DWIDTH/8-1 downto 0);
+    Bus2IP_RdCE                    : in  std_logic_vector(C_NUM_REG-1 downto 0);
+    Bus2IP_WrCE                    : in  std_logic_vector(C_NUM_REG-1 downto 0);
+    IP2Bus_Data                    : out std_logic_vector(C_SLV_DWIDTH-1 downto 0);
+    IP2Bus_RdAck                   : out std_logic;
+    IP2Bus_WrAck                   : out std_logic;
+    IP2Bus_Error                   : out std_logic
+    -- DO NOT EDIT ABOVE THIS LINE ---------------------
+  );
+
+  attribute MAX_FANOUT : string;
+  attribute SIGIS : string;
+
+  attribute SIGIS of Bus2IP_Clk    : signal is "CLK";
+  attribute SIGIS of Bus2IP_Resetn : signal is "RST";
+
+end entity user_logic;
+
+------------------------------------------------------------------------------
+-- Architecture section
+------------------------------------------------------------------------------
+
+architecture IMP of user_logic is
+
+  --USER signal declarations added here, as needed for user logic
+  signal controller_next				: std_logic_vector(C_SLV_DWIDTH-1 downto 0);
+  signal temp_write_reg					: std_logic_vector(C_SLV_DWIDTH-1 downto 0) := (others => '0');
+  signal interrupt_reg					: std_logic_vector(0 downto 0) := (others => '0');
+  signal interrupt_next					: std_logic_vector(0 downto 0);
+  signal ack_sig						: std_logic := '0';
+  
+  ------------------------------------------
+  -- Signals for user logic slave model s/w accessible register example
+  ------------------------------------------
+  signal controller_reg					: std_logic_vector(C_SLV_DWIDTH-1 downto 0) := (others => '0');
+  signal slv_reg_write_sel              : std_logic_vector(0 to 0);
+  signal slv_reg_read_sel               : std_logic_vector(0 to 0);
+  signal slv_ip2bus_data                : std_logic_vector(C_SLV_DWIDTH-1 downto 0);
+  signal slv_read_ack                   : std_logic;
+  signal slv_write_ack                  : std_logic;
+
+begin
+
+  --USER logic implementation added here
+
+  ------------------------------------------
+  -- Example code to read/write user logic slave model s/w accessible registers
+  -- 
+  -- Note:
+  -- The example code presented here is to show you one way of reading/writing
+  -- software accessible registers implemented in the user logic slave model.
+  -- Each bit of the Bus2IP_WrCE/Bus2IP_RdCE signals is configured to correspond
+  -- to one software accessible register by the top level template. For example,
+  -- if you have four 32 bit software accessible registers in the user logic,
+  -- you are basically operating on the following memory mapped registers:
+  -- 
+  --    Bus2IP_WrCE/Bus2IP_RdCE   Memory Mapped Register
+  --                     "1000"   C_BASEADDR + 0x0
+  --                     "0100"   C_BASEADDR + 0x4
+  --                     "0010"   C_BASEADDR + 0x8
+  --                     "0001"   C_BASEADDR + 0xC
+  -- 
+  ------------------------------------------
+  slv_reg_write_sel <= Bus2IP_WrCE(0 downto 0);
+  slv_reg_read_sel  <= Bus2IP_RdCE(0 downto 0);
+  slv_write_ack     <= Bus2IP_WrCE(0);
+  slv_read_ack      <= Bus2IP_RdCE(0);
+
+  -- implement slave model software accessible register(s)
+  SLAVE_REG_WRITE_PROC : process( Bus2IP_Clk ) is
+  begin
+
+    if Bus2IP_Clk'event and Bus2IP_Clk = '1' then
+      -- reset?
+	  if Bus2IP_Resetn = '0' then
+        controller_reg <= (others => '0');
+        temp_write_reg <= (others => '0');
+		interrupt_reg <= '0';
+	
+	  -- load
+      else
+	    -- load next value
+		controller_reg <= controller_next;
+		interrupt_reg <= interrupt_next;
+	  
+	    -- the register should never be written to
+        case slv_reg_write_sel is
+          when "1" =>
+            for byte_index in 0 to (C_SLV_DWIDTH/8)-1 loop
+              if ( Bus2IP_BE(byte_index) = '1' ) then
+                temp_write_reg(byte_index*8+7 downto byte_index*8) <= Bus2IP_Data(byte_index*8+7 downto byte_index*8);
+              end if;
+            end loop;
+			
+			-- write acknowledge bit
+			controller_reg(ACK_BIT) <= temp_write_reg(ACK_BIT);
+          when others => null;
+        end case;
+      end if;
+    end if;
+
+  end process SLAVE_REG_WRITE_PROC;
+
+  -- implement slave model software accessible register(s) read mux
+  SLAVE_REG_READ_PROC : process( slv_reg_read_sel, controller_reg ) is
+  begin
+
+    case slv_reg_read_sel is
+      when "1" => slv_ip2bus_data <= controller_reg;
+      when others => slv_ip2bus_data <= (others => '0');
+    end case;
+
+  end process SLAVE_REG_READ_PROC;
+
+  ------------------------------------------
+  -- Example code to drive IP to Bus signals
+  ------------------------------------------
+  IP2Bus_Data  <= slv_ip2bus_data when slv_read_ack = '1' else
+                  (others => '0');
+
+  IP2Bus_WrAck <= slv_write_ack;
+  IP2Bus_RdAck <= slv_read_ack;
+  IP2Bus_Error <= '0';
+  
+  ------------------------------------------
+  -- INTERNAL LOGIC
+  ------------------------------------------
+  -- The acknowledge bit in the controller register will only ever be high for one clock cycle,
+  -- when written to from the BUS (see the SLAVE_REG_WRITE_PROC process).
+  ack_sig <= '0';
+  
+  
+  ------------------------------------------
+  -- NEXT STATE LOGIC
+  ------------------------------------------
+  -- These signals are read directly from the PMOD (see [system.ucf]).
+  controller_next <= (UP_BIT		=> PMOD_up,
+					  DOWN_BIT		=> PMOD_down,
+					  LEFT_BIT		=> PMOD_left,
+					  RIGHT_BIT		=> PMOD_right,
+					  BUTTON_BIT	=> PMOD_button,
+					  ACK_BIT		=> ack_sig,
+					  others		=> '0');
+					  
+  -- The interrupt bit will function normally (go high on a transition in user input on the
+  -- controller) when the acknowledge bit is HIGH. When the acknowledge bit is LOW, however, it
+  -- will retain its value; it is level-sensitive.
+  interrupt_next <= ~(controller_reg = controller_next) when (controller_reg(ACK_BIT) = '1') else
+					'1' when (controller_reg(ACK_BIT) = '0') else
+  
+					  
+  ------------------------------------------
+  -- OUTPUT LOGIC
+  ------------------------------------------
+  CTRL_int <= interrupt_reg;
+  
+  
+end IMP;
